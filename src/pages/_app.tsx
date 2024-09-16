da87c04bf1949a2d44503f94832960e65972a245
Bradley Greaves <Bradley.Greaves@mmtdigital.co.uk>
2024-09-16 09:43:36 +0100

    Updated styling for cards, inc. font.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import useFetchCountries from '../hooks/useFetchCountries';
 import CountryCard from '../components/CountryCard';
 import styles from '../styles/HomePage.module.css'
+import Head from 'next/head';
+import '../styles/globals.css';
 
 const HomePage = () => {
   const { countries, loading, error } = useFetchCountries();
@@ -9,11 +11,16 @@ const HomePage = () => {
   if (error) return <p>{error}</p>;
 
   return (
+    <>
+    <Head>
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet"/>
+    </Head>
     <div className={styles.countryGrid}>
       {countries.map((country) => (
         <CountryCard key={country.name.common} country={country} />
       ))}
     </div>
+  </>
   );
 };
 
